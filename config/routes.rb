1598f95607829c169346c96bf6c511a99314d270
Nikolaus124 <deeven.dark@gmail.com>
2020-11-11 11:48:47 +0200

    Datepicker and change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #root 'spaces#index'
+  resources :spaces
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
