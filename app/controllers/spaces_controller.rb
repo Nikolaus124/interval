2032a138f14e2f9d148df92c73f727cfb4dd38ce
Nikolaus124 <deeven.dark@gmail.com>
2020-11-13 15:53:21 +0200

    Created input form

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,9 +4,23 @@ class SpacesController < ApplicationController
     @patients = Patient.all
   end
 
-  def show
-    @space = Space.find(params[:id])
-    @doctor = Doctor.find(params[:id])
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to @space
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:patient_id, :doctor_id, :start_time, :end_time, :create_date)
   end
 
 end
