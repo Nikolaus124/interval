71f2972fdede51547eb5a38bcf97658fcdfb5740
Nikolaus124 <deeven.dark@gmail.com>
2020-11-26 11:15:33 +0200

    Final Front

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,5 @@
 class Doctor < ApplicationRecord
-
+  def self.search(dcr)
+    where(["doctor_id = ? AND create_date = ?", dcr.to_s])
+  end
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -2,34 +2,21 @@ class Space < ApplicationRecord
   MIN_LENGTH = 5
   MAX_LENGTH = 30
 
-  validates :start_time, :end_time, length: {is: 5}, :presence => {:message => "must be not null."}
+  validates :create_date, :patient_id, :doctor_id, presence: true
+  validates :start_time, :end_time, presence: true
 
   belongs_to :patient
   belongs_to :doctor
   validate :actual_date, :actual_time, :time_inv, :division, :length_interval, :check_d_h
   validates_with SpaceValidator
 
-=begin
-  def self.search(search)
-    if search
-      doctor = Doctor.find_by(position: search)
-      if doctor
-        where(doctor_id: doctor)
-      else
-        Space.all
-      end
-    else
-      Space.all
-    end
-  end
-=end
 
   def self.search(dcr, dt)
     where(["doctor_id = ? AND create_date = ?", dcr.to_s, dt.to_s])
   end
 
   def actual_date
-    errors.add(:base, "incorrect date") if create_date < Time.now.to_date
+    errors.add(:base, "Incorrect date") if create_date < Time.now.to_date
   end
 
   def actual_time
@@ -37,7 +24,7 @@ class Space < ApplicationRecord
   end
 
   def time_inv
-    errors.add(:base, "REVERS NOT NORMAL") if Time.parse(start_time) > Time.parse(end_time)
+    errors.add(:base, "Start time must be earlier than end time") if Time.parse(start_time) > Time.parse(end_time)
   end
 
   def division
@@ -60,10 +47,10 @@ class Space < ApplicationRecord
     if (d_s.hour * 60 + d_s.min) > (s.hour * 60 + s.min)
       errors.add(:start_time, "should be not earlier #{doctor.begin}")
     end
-    if (d_e.hour * 60 + d_e.min) <= (s.hour * 60 + s.min)
+    if (d_e.hour * 60 + d_e.min) < (s.hour * 60 + s.min)
       errors.add(:start_time, "should be earlier #{doctor.end}")
     end
-    if (d_e.hour * 60 + d_e.min) <= (e.hour * 60 + e.min)
+    if (d_e.hour * 60 + d_e.min) < (e.hour * 60 + e.min)
       errors.add(:base, "Space should be between #{doctor.begin}-#{doctor.end}")
     end
   end
